f8d6565d5d93634c64400b72e7dc9143d87d4e03
vinicius <niviciusdev@gmail.com>
2025-05-05 00:30:24 -0300

    Add gist and comment functionality with data models

---
+++ test/support/fixtures/gists_fixtures.ex
@@ -0,0 +1,36 @@
+defmodule Gelixir.GistsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Gelixir.Gists` context.
+  """
+
+  @doc """
+  Generate a gist.
+  """
+  def gist_fixture(attrs \\ %{}) do
+    {:ok, gist} =
+      attrs
+      |> Enum.into(%{
+        description: "some description",
+        markup_text: "some markup_text",
+        name: "some name"
+      })
+      |> Gelixir.Gists.create_gist()
+
+    gist
+  end
+
+  @doc """
+  Generate a saved_gist.
+  """
+  def saved_gist_fixture(attrs \\ %{}) do
+    {:ok, saved_gist} =
+      attrs
+      |> Enum.into(%{
+
+      })
+      |> Gelixir.Gists.create_saved_gist()
+
+    saved_gist
+  end
+end
